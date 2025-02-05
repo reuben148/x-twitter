3d11ed0d77da80dcadc2a4e157a9bfe991c1101a
Akanimoh12 <akanimohjohnson2003@gmail.com>
2025-02-05 23:28:31 +0100

    added WhoToFollow component

--- src/components/Home/RightNav.jsx
+++ src/components/Home/RightNav.jsx
@@ -1,11 +1,14 @@
 import SubToPremium from './SubToPremium';
 import WhatsHappening from './WhatsHappening';
+import WhoToFollow from './WhoToFollow';
 
 export default function RightNav() {
   return (
     <div>
      <SubToPremium/>
      <WhatsHappening/> 
+     <WhoToFollow/>
+
     </div>
   )
 }
