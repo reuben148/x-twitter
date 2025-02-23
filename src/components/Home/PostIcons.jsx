f767318828864e55e9ef38c1a0cb34e35da8ff00
Akanimoh12 <akanimohjohnson2003@gmail.com>
2025-02-23 04:48:15 +0100

    updated

--- src/components/Home/PostIcons.jsx
+++ src/components/Home/PostIcons.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export default function PostIcons() {
   return (
     <div>
-      <h1>Hello</h1>
+      
+      <div>
+        
+      </div>
+      
     </div>
   )
 }
